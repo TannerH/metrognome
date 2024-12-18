8ed91d16dc0aad6768fec7e752d32e7f78ec0a13
Tanner Hoffman <tanner.r.hoffman@gmail.com>
2024-12-18 12:54:25 -0600

    checkpoint before adding menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {ButtonModule} from 'primeng/button';
 import {SliderModule} from 'primeng/slider';
+import {MetronomeComponent} from './metronome/metronome.component';
+import {MenuComponent} from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
@@ -9,68 +11,13 @@ import {SliderModule} from 'primeng/slider';
   imports: [
     FormsModule,
     ButtonModule,
-    SliderModule
+    SliderModule,
+    MetronomeComponent,
+    MenuComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: []
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  private audio: HTMLAudioElement;
-  protected bpm = 90;
-  private intervalId: number | null = null;
-
-  ngOnInit(): void {
-    this.audio = new Audio('Perc_Can_hi.wav');
-    if (this.audio.error) {
-      console.error("Error loading audio file.");
-    }
-    // this.start();
-  }
-
-
-  start(): void {
-    if (this.intervalId !== null) {
-      console.warn("Metronome is already running.");
-      return;
-    }
-
-    const intervalMs = 60000 / this.bpm; // Calculate interval in milliseconds
-    setInterval(() => {
-    })
-    // @ts-ignore
-    this.intervalId = setInterval(() => {
-      this.audio.currentTime = 0; // Reset the audio to the beginning
-      console.log("BEEP");
-      this.audio.play().catch((error) => console.error("Error playing sound:", error));
-    }, intervalMs);
-
-    console.log(`Metronome started at ${this.bpm} BPM.`);
-  }
-
-  stop(): void {
-    if (this.intervalId === null) {
-      console.warn("Metronome is not running.");
-      return;
-    }
-
-    clearInterval(this.intervalId);
-    this.intervalId = null;
-    console.log("Metronome stopped.");
-  }
-
-  setBPM(newBPM: number): void {
-    if (newBPM <= 0) {
-      console.error("BPM must be greater than 0.");
-      return;
-    }
-
-    this.bpm = newBPM;
-    if (this.intervalId !== null) {
-      this.stop();
-      this.start();
-    }
-
-    console.log(`BPM set to ${this.bpm}.`);
-  }
 }
